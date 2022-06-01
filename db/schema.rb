d306a49fcbc8f352546e78a508b0097a7456c9c5
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-01 16:24:22 +0100

    Generate Billionaires

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_01_151106) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_152213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "billionaires", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "image", null: false
+    t.float "price", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
