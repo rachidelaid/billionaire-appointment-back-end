97fe3c414dd2c86d5f9b605fbb9da6528ec8e1e0
orozcoding <orozcoding@gmail.com>
2022-06-02 04:57:58 -0400

    Add billionaires seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Billionaire.create(
+  name: 'Elon Musk',
+  title: "CEO at SpaceX and Tesla, Inc.",
+  price: 3250,
+  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_elon.png?raw=true',
+  description: "Elon Musk is a business magnate and investor. He is the founder, CEO, and Chief Engineer at SpaceX; angel investor, CEO, and Product Architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. With an estimated net worth of around US$265 billion as of May 2022, Musk is the wealthiest person in the world according to both the Bloomberg Billionaires Index and the Forbes real-time billionaires list.")
+Billionaire.create(
+  name: 'Jeff Bezos',
+  title: 'Founder of Amazon',
+  price: 2650,
+  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true',
+  description: "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.")
+Billionaire.create(
+  name: 'Ariel Camus',
+  title: 'Founder & CEO at Microverse',
+  price: 4350,
+  image: "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_ariel.png?raw=true",
+  description: "The Argentine-born has founded multiple projects, including the one where is currently CEO: Microverse, a school that enables job-opportunities for talent around the world. Ariel has lived and worked in 7 different countries including Spain, Argentina, USA, and Vietnam and has been coding since he was 12. He previously founded Tourist Eye, a travel app with 1MM+ users acquired by Lonely Planet.")
+  Billionaire.create(
+  name: 'Julia Koch', 
+  title: 'President of David H. Koch Foundation', 
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_julia.png', 
+  price: 2900, 
+  description: 'Julia Koch is an American socialite, billionaire, philanthropist, and one of the richest women in the world. She inherited her fortune from her husband, David Koch, who died in 2019. As of January 2020, Forbes estimates her and her family\'s net worth at $43 billion, while Bloomberg estimates it at $61.6 billion.')
+  Billionaire.create(
+  name: 'Alice Walton', 
+  title: 'Heiress, Walton family fortune', 
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_alice.png', 
+  price: 2800, 
+  description: 'Alice Walton is an American heiress to the fortune of Walmart. In September 2016, she owned over US$11 billion in Walmart shares.[3] As of March 2022, Walton has a net worth of $60.5 billion, making her the 20th-richest person, and the second richest woman in the world according to Bloomberg Billionaires Index.')
+  Billionaire.create(
+  name: 'Jack Ma', 
+  title: 'Co-founder of Yunfeng Capital', 
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_jack.png', 
+  price: 2290, 
+  description: 'Jack Ma is a Chinese business magnate, investor and philanthropist. He is the co-founder and former executive chairman of Alibaba Group, a multinational technology conglomerate. In addition, he co-founded Yunfeng Capital, a Chinese private equity firm. Ma is a strong proponent of an open and market-driven economy.')
+  Billionaire.create(
+  name: 'Francoise Meyers',
+  title: "Chairwoman at L'Oréal",
+  price: 3900,
+  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_francoise.png?raw=true',
+  description: "Francoise Meyers is a French businesswoman, philanthropist, writer, pianist and billionaire heiress, the richest woman in the world, with an estimated net worth of US$75.3 billion as of March 2022, according to Forbes. She is the only child, heiress of Liliane Bettencourt and granddaughter of L'Oréal founder Eugène Schueller. Her mother died in September 2017, after which her fortune tripled with her investments through her family holding company, Tethys Invest, and the high valuation of L'Oréal shares on the stock exchange.")
+  Billionaire.create( 
+    name: 'Changpeng Zhao',
+    title: 'Founder & CEO at Binance',
+    price: 3500,
+    image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_chang.png?raw=true',
+    description: "Changpeng Zhao commonly known as 'CZ', is a Chinese-Canadian business executive. Zhao is the founder and CEO of Binance, the world's largest cryptocurrency exchange by trading volume as of April 2018. CZ was born in Jiangsu, China and moved to Canada at a young age with his family.")
+  Billionaire.create( 
+    name: 'Mark Zuckerberg',
+    title: 'Co-founder & CEO of Meta Platforms',
+    price: 2980,
+    image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_mark.png?raw=true',
+    description: 'Mark Zuckerberg is an American media magnate, internet entrepreneur, and philanthropist. He is known for co-founding the social media website Facebook and its parent company Meta Platforms (formerly Facebook, Inc.), of which he is the chairman, chief executive officer, and controlling shareholder.')
+  Billionaire.create( 
+    name: 'Bill Gates',
+    title: 'Co-founder of Microsoft',
+    price: 3120,
+    image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_bill.png?raw=true',
+    description: 'Billgates is an American business magnate, software developer, investor, author, and philanthropist. He is a co-founder of Microsoft, along with his late childhood friend Paul Allen. During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president and chief software architect, while also being the largest individual shareholder until May 2014. He was a major entrepreneur of the microcomputer revolution of the 1970s and 1980s.')
