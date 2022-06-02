b9c47c69c06ee055d95b61a06490eb52080a3af9
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-02 10:05:33 +0100

    Fix linters

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     name { 'Pedro Guerreiro' }
     username { 'pepedro1' }
-    role {'user'}
+    role { 'user' }
     email { 'pedro@gmail.com' }
     password { '123456' }
-    
   end
 end
