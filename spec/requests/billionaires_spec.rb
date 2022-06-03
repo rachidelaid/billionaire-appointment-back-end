a143d3411886f07b7cac36a9662a9d7140677363
matovu-farid <matovu90@gmail.com>
2022-06-03 20:19:29 +0300

    Add show page test

--- spec/requests/billionaires_spec.rb
+++ spec/requests/billionaires_spec.rb
@@ -18,11 +18,6 @@ RSpec.describe '/billionaires', type: :request do
   let(:invalid_billionaire) {build :billionaire, name: nil}
   let(:invalid_attributes) {invalid_billionaire.attributes}
   
-
-  # This should return the minimal set of values that should be in the headers
-  # in order to pass any filters (e.g. authentication) defined in
-  # BillionairesController, or in your router and rack
-  # middleware. Be sure to keep this updated too.
   
 
   describe 'GET /index' do
@@ -33,88 +28,12 @@ RSpec.describe '/billionaires', type: :request do
     end
   end
 
-  # describe 'GET /show' do
-  #   it 'renders a successful response' do
-  #     billionaire = Billionaire.create! valid_attributes
-  #     get billionaire_url(billionaire), as: :json
-  #     expect(response).to be_successful
-  #   end
-  # end
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      valid_billionaire.save
+      get api_billionaire_url(valid_billionaire), as: :json
+      expect(response).to be_successful
+    end
+  end
 
-  # describe 'POST /create' do
-  #   context 'with valid parameters' do
-  #     it 'creates a new Billionaire' do
-  #       expect do
-  #         post billionaires_url,
-  #              params: { billionaire: valid_attributes }, headers: valid_headers, as: :json
-  #       end.to change(Billionaire, :count).by(1)
-  #     end
-
-  #     it 'renders a JSON response with the new billionaire' do
-  #       post billionaires_url,
-  #            params: { billionaire: valid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:created)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-
-  #   context 'with invalid parameters' do
-  #     it 'does not create a new Billionaire' do
-  #       expect do
-  #         post billionaires_url,
-  #              params: { billionaire: invalid_attributes }, as: :json
-  #       end.to change(Billionaire, :count).by(0)
-  #     end
-
-  #     it 'renders a JSON response with errors for the new billionaire' do
-  #       post billionaires_url,
-  #            params: { billionaire: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-  # end
-
-  # describe 'PATCH /update' do
-  #   context 'with valid parameters' do
-  #     let(:new_attributes) do
-  #       skip('Add a hash of attributes valid for your model')
-  #     end
-
-  #     it 'updates the requested billionaire' do
-  #       billionaire = Billionaire.create! valid_attributes
-  #       patch billionaire_url(billionaire),
-  #             params: { billionaire: new_attributes }, headers: valid_headers, as: :json
-  #       billionaire.reload
-  #       skip('Add assertions for updated state')
-  #     end
-
-  #     it 'renders a JSON response with the billionaire' do
-  #       billionaire = Billionaire.create! valid_attributes
-  #       patch billionaire_url(billionaire),
-  #             params: { billionaire: new_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:ok)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-
-  #   context 'with invalid parameters' do
-  #     it 'renders a JSON response with errors for the billionaire' do
-  #       billionaire = Billionaire.create! valid_attributes
-  #       patch billionaire_url(billionaire),
-  #             params: { billionaire: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-  # end
-
-  # describe 'DELETE /destroy' do
-  #   it 'destroys the requested billionaire' do
-  #     billionaire = Billionaire.create! valid_attributes
-  #     expect do
-  #       delete billionaire_url(billionaire), headers: valid_headers, as: :json
-  #     end.to change(Billionaire, :count).by(-1)
-  #   end
-  # end
 end
