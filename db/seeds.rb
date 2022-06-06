c99fe62ceee8a63c112621a595efc2185078e5d8
orozcoding <orozcoding@gmail.com>
2022-06-06 07:22:11 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ Billionaire.create(
   name: 'Elon Musk',
   title: "CEO at SpaceX and Tesla, Inc.",
   price: 3250,
-  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_elon.png?raw=true',
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_elon.png',
   description: "Elon Musk is a business magnate and investor. He is the founder, CEO, and Chief Engineer at SpaceX; angel investor, CEO, and Product Architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. With an estimated net worth of around US$265 billion as of May 2022, Musk is the wealthiest person in the world according to both the Bloomberg Billionaires Index and the Forbes real-time billionaires list.")
 Billionaire.create(
   name: 'Jeff Bezos',
   title: 'Founder of Amazon',
   price: 2650,
-  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true',
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_jeff.png',
   description: "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.")
 Billionaire.create(
   name: 'Ariel Camus',
   title: 'Founder & CEO at Microverse',
   price: 4350,
-  image: "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_ariel.png?raw=true",
+  image: "https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_ariel.png",
   description: "The Argentine-born has founded multiple projects, including the one where is currently CEO: Microverse, a school that enables job-opportunities for talent around the world. Ariel has lived and worked in 7 different countries including Spain, Argentina, USA, and Vietnam and has been coding since he was 12. He previously founded Tourist Eye, a travel app with 1MM+ users acquired by Lonely Planet.")
   Billionaire.create(
   name: 'Julia Koch', 
@@ -46,19 +46,19 @@ Billionaire.create(
   name: 'Francoise Meyers',
   title: "Chairwoman at L'Oréal",
   price: 3900,
-  image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_francoise.png?raw=true',
+  image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_francoise.png',
   description: "Francoise Meyers is a French businesswoman, philanthropist, writer, pianist and billionaire heiress, the richest woman in the world, with an estimated net worth of US$75.3 billion as of March 2022, according to Forbes. She is the only child, heiress of Liliane Bettencourt and granddaughter of L'Oréal founder Eugène Schueller. Her mother died in September 2017, after which her fortune tripled with her investments through her family holding company, Tethys Invest, and the high valuation of L'Oréal shares on the stock exchange.")
   Billionaire.create( 
     name: 'Changpeng Zhao',
     title: 'Founder & CEO at Binance',
     price: 3500,
-    image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_chang.png?raw=true',
+    image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_chang.png',
     description: "Changpeng Zhao commonly known as 'CZ', is a Chinese-Canadian business executive. Zhao is the founder and CEO of Binance, the world's largest cryptocurrency exchange by trading volume as of April 2018. CZ was born in Jiangsu, China and moved to Canada at a young age with his family.")
   Billionaire.create( 
     name: 'Mark Zuckerberg',
     title: 'Co-founder & CEO of Meta Platforms',
     price: 2980,
-    image: 'https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_mark.png?raw=true',
+    image: 'https://raw.githubusercontent.com/orozCoding/billionares-pictures/main/round_pictures/pc_mark.png',
     description: 'Mark Zuckerberg is an American media magnate, internet entrepreneur, and philanthropist. He is known for co-founding the social media website Facebook and its parent company Meta Platforms (formerly Facebook, Inc.), of which he is the chairman, chief executive officer, and controlling shareholder.')
   Billionaire.create( 
     name: 'Bill Gates',
