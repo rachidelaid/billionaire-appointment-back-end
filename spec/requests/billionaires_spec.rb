8b88472add8c3c2a758e5beb2d6ca6b45155e4dd
orozcoding <orozcoding@gmail.com>
2022-06-06 10:40:39 -0400

    Add billionaire test with valid parameters

--- spec/requests/billionaires_spec.rb
+++ spec/requests/billionaires_spec.rb
@@ -12,10 +12,21 @@ RSpec.describe '/billionaires', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Billionaire. As you add validations to Billionaire, be sure to
   # adjust the attributes here as well.
+
+  
+
   let(:valid_billionaire) { build :billionaire }
   let(:valid_attributes) { valid_billionaire.attributes }
   let(:invalid_billionaire) { build :billionaire, name: nil }
   let(:invalid_attributes) { invalid_billionaire.attributes }
+
+  let(:application) { FactoryBot.create(:application) }
+  let(:current_user) { FactoryBot.create(:user, role: 'admin') }
+  let(:access_token) { FactoryBot.create(:access_token, application: application, resource_owner_id: current_user.id) }
+
+  let(:valid_headers) do
+    {"Authorization": "Bearer #{access_token.token}"}
+  end
 
   describe 'GET /index' do
     it 'renders a successful response' do
@@ -30,6 +41,18 @@ RSpec.describe '/billionaires', type: :request do
       valid_billionaire.save
       get api_billionaire_url(valid_billionaire), as: :json
       expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Billionaire' do
+      post api_billionaires_url,
+            params: { billionaire: valid_attributes },
+            headers: valid_headers,
+            as: :json
+        expect(response).to have_http_status(:created)
+      end
     end
   end
 end
