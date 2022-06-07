8b72ace3a68db1c856dca0b540267994bac900c5
rachidelaid <rachidelaid@gmail.com>
2022-06-07 13:16:14 +0100

    run rubocop

--- app/controllers/custom_tokens_controller.rb
+++ app/controllers/custom_tokens_controller.rb
@@ -3,24 +3,21 @@ class CustomTokensController < Doorkeeper::TokensController
 
   def create
     headers.merge!(authorize_response.headers)
-    
-    response = createResponse(authorize_response.body)
+
+    response = create_response(authorize_response.body)
     render json: response, status: authorize_response.status
   rescue Doorkeeper::Errors => e
     handle_token_exception(e)
   end
 
   private
-  def createResponse(response)
-    if response[:error].to_s == "invalid_client"
-      return { error: "invalid client id or secret" }
-    end
 
-    if response[:error].to_s == "invalid_grant"
-      return { error: "wrong username or password" }
-    end
+  def create_response(response)
+    return { error: 'invalid client id or secret' } if response[:error].to_s == 'invalid_client'
+
+    return { error: 'wrong username or password' } if response[:error].to_s == 'invalid_grant'
 
     user = User.find_by(id: Doorkeeper::AccessToken.find_by(token: response['access_token']).resource_owner_id)
-    return { token: response, user: }
+    { token: response, user: }
   end
 end
