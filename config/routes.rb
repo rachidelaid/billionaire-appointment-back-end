d69d62892c53d0e52def66dcc41aa6363a6bf28e
matovu-farid <matovu90@gmail.com>
2022-06-10 13:15:14 +0300

    Exclude the update from appointments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     resources :users, only: :create
     resources :billionaires, except: :update
-    resources :appointments
+    resources :appointments, except: :update
   end
 
   use_doorkeeper do
