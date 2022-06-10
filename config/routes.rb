fc8e1226f01226cd05407f9d6a8010d81ed08368
matovu-farid <matovu90@gmail.com>
2022-06-10 13:04:48 +0300

    Remove update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: 'json' } do
     resources :users
-    resources :billionaires
+    resources :billionaires, except: [:update]
     resources :appointments
   end
 
